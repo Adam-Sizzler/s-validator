396a513f4c559995b0064d2f7f0e239c5fb6aa76
Adam <cortez24rus@gmail.com>
2026-03-07 16:43:17 +0700

    test

--- src/shared/constants/supported-versions.ts
+++ src/shared/constants/supported-versions.ts
@@ -8,11 +8,11 @@ export const LATEST_SUPPORTED_VERSION = 'latest'
 
 export const SUPPORTED_VERSION_OPTIONS: VersionOption[] = [
     {
-        label: '1.11.13',
+        label: '1.11.13:latest_ios',
         value: DEFAULT_SUPPORTED_VERSION
     },
     {
-        label: 'latest stable',
+        label: '1.13.1:latest_git',
         value: LATEST_SUPPORTED_VERSION
     }
 ]
